3558e554ec576d695908affeb5e27356dfc6c97d
maciej.wolejszo <maciej.wolejszo@gmail.com>
2017-04-13 00:43:18 +0200

    The pillars of object-oriented programming in C # Lesson - 04

---
+++ HelloWorldSolution/HelloWorld/Dziedziczenie/Pojazd.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Dziedziczenie
+{
+    class Pojazd
+    {
+        private string _marka;
+        private string _model;
+        private int _ileKol;
+        private int _cena;
+
+
+        public string Marka
+        {
+            get
+            {
+                return _marka;
+            }
+            set
+            {
+                _marka = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+
+        public int IleKol
+        {
+            get
+            {
+                return _ileKol;
+            }
+            set
+            {
+                _ileKol = value;
+            }
+        }
+        public int Cena
+        {
+            get
+            {
+                return _cena;
+            }
+            set
+            {
+                _cena = value;
+            }
+        }
+
+        public Pojazd(string marka,string model)
+        {
+            Marka = marka;
+            Model = model;
+        }
+
+        public Pojazd(string marka, string model, int ileKol, int cena)
+        {
+            Marka = marka;
+            Model = model;
+            IleKol = ileKol;
+            Cena = cena;
+        }
+
+        public Pojazd() { }
+
+        public Pojazd(string marka, int ileKol, int cena)
+        {
+            Marka = marka;
+            IleKol = ileKol;
+            Cena = cena;
+        }
+
+        public virtual void Jedz()
+        {
+            Console.WriteLine("Pojazd Jedzie...");
+        }
+    }
+}
---
+++ HelloWorldSolution/HelloWorld/Dziedziczenie/Samochod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Dziedziczenie
+{
+    class Samochod:Pojazd
+    {
+        private string _jakiePaliwo;
+        public Samochod(string marka, string model, int ileKol, int cena, string jakiePaliwo)
+            : base(marka, model, ileKol, cena)
+        {
+            JakiePaliwo = jakiePaliwo;
+        }
+        //public Samochod(){}
+        public string JakiePaliwo()
+        {
+            get
+            {
+                return _jakiePaliwo;
+            }
+            set
+            {
+                _jakiePaliwo = Value;
+            }
+        }
+
+
+
+        public override void Jedz()
+        {
+            Console.WriteLine("Samochod jedzie i spala 8litrów/100kn...");
+        }
+    }
