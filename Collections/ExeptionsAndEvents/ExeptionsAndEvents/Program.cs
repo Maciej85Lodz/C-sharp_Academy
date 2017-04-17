5633bf689e6de3746ad894560862694ef6ec8d7c
maciej.wolejszo <maciej.wolejszo@gmail.com>
2017-04-17 22:29:03 +0200

    Exceptiions and Events - lesson 07 Part 1

--- Collections/ExeptionsAndEvents/ExeptionsAndEvents/Program.cs
+++ Collections/ExeptionsAndEvents/ExeptionsAndEvents/Program.cs
@@ -10,6 +10,23 @@ namespace ExeptionsAndEvents
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int[] myTab = new int[2];
+                myTab[0] = 1;
+                myTab[1] = 2;
+                int element = myTab[10];
+            }
+
+            catch(IndexOutOfRangeException exception)
+            {
+                Console.WriteLine("Error: " + exception.Message);
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine("Other exception: " + exception.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
