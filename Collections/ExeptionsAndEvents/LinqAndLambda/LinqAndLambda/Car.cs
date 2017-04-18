29013d05a7b672dda0b8588d7d1e0bd944135aa0
maciej.wolejszo <maciej.wolejszo@gmail.com>
2017-04-18 20:34:24 +0200

    BestPractices - lesson 09

---
+++ Collections/ExeptionsAndEvents/LinqAndLambda/LinqAndLambda/Car.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLambda
+{
+    class Car
+    {
+        private string _brand;
+        private string _color;
+        private int _year;
+
+        public string Brand
+        {
+            get
+            {
+                return _brand;
+            }
+            set
+            {
+                if (value != null)
+                    _brand = value;
+            }
+        }
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                if (value != null)
+                    _color = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                if (value != null)
+                    _year = value;
+            }
+        }
+        public Car() { }
+        public Car(string brand, string color)
+        {
+            Brand = brand;
+            Color = color;
+
+        }
+        public void Drive()
+        {
+            Console.WriteLine("Auto jedzie");
+            checkOil();
+        }
+
+        private void checkOil()
+        {
+            Console.WriteLine("Sprawdzam stan oleju..., OK");
+        }
+
+
+    }
+}
---
+++ Collections/ExeptionsAndEvents/LinqAndLambda/LinqAndLambda/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car myNewCar = new Car();
+            myNewCar.Drive();
+            myNewCar.Color = "White";
+            myNewCar.Brand = "Mercedes";
+
+
+
+
+
+
+            //        var cars = new List<Car>
+            //        {
+            //            new Car { Brand = "BMW", COLOR = "Black", Year = 1990 };
+            //            new Car { Brand = "BMW", COLOR = "Red", Year = 1992 };
+            //            new Car { Brand = "Audi", COLOR = "Blue", Year = 1998 };
+            //            new Car { Brand = "Ford", COLOR = "Black", Year = 1990 };
+            //        };
+
+            //var bmws = from car in cars
+            //           where car.Brand == "BMW"
+
+
+            Console.ReadKey();
+        }
+        
+
+}
+}
